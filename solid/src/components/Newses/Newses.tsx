5c27e538e7445c4ee52ff0c3ef547dd6c5c39412
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2023-11-07 01:42:24 +0600

    implement the logic to news no in the page

--- solid/src/components/Newses/Newses.tsx
+++ solid/src/components/Newses/Newses.tsx
@@ -3,15 +3,15 @@ import { TNews } from "@/components/Newses/News";
 
 import News from "./News";
 
-export default (p: { newses: TNews[] }) => {
+export default (p: { newses: TNews[]; page?: number; total?: number }) => {
     return (
         <div class="flex flex-col gap-1">
             <For each={p.newses}>
                 {(news: TNews, i) => (
                     <News
+                        no={(p.total || 30) * ((p.page || 1) - 1) + i() + 1}
                         id={news.id}
                         title={news.title}
-                        no={i() + 1}
                         url={news.url}
                         domain={news.domain}
                         points={news.points}
